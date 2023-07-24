dd2a65bf7cbec0ab1e67d27ebf4f343279bfc6e4
moises valdez <shmoi@moisess-MacBook-Pro.local>
2023-07-24 13:17:17 -0700

    Try and Catch for the guessing game

--- guess_number.py
+++ guess_number.py
@@ -4,11 +4,14 @@ def guess(x):
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
-        guess = int(input(f'Guess a number between 1 and {x}: '))
-        if guess < random_number:
-            print('Sorry, guess again. Too low.')
-        elif guess > random_number:
-            print('Sorry, guess again. Too high.')
+        try:
+            guess = int(input(f'Guess a number between 1 and {x}: '))
+            if guess < random_number:
+                print('Sorry, guess again. Too low.')
+            elif guess > random_number:
+                print('Sorry, guess again. Too high.')
+        except ValueError:
+            print('Invalid input. Please enter a valid number.')
     print(f'Yay, congrats. You have guessed the number {random_number} correctly!!')
 
 guess(10)
